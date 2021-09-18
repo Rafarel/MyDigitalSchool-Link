0daf36983637313dece1a7e434af9ff92b229479
Raphaël Roux <rafarel007@hotmail.com>
2021-09-18 13:35:28 +0200

    GameManager Singleton + ScoreManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,41 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
     
+    public RupeesManager Rupees { get; private set; }
+    
+    public ScoreManager Score { get; private set; }
+
+    public int scoreToWin;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Rupees = GetComponent<RupeesManager>();
+        Score = GetComponent<ScoreManager>();
+
+
+        Score.Changed += ScoreChangedHandler;
+    }
+
+    private void ScoreChangedHandler(object sender, EventArgs args)
+    {
+        Debug.Log(Score.Value);
+        
+        if (Score.Value >= scoreToWin)
+        {
+            Debug.Log("Game Win!");
+        }
+    }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+
+public class ScoreManager : MonoBehaviour
+{
+    private GameManager m_Game;
+    public int Value { get; private set; }
+
+    public event EventHandler<EventArgs> Changed;
+    
+    private void Awake()
+    {
+        m_Game = GameManager.Instance;
+
+        m_Game.Rupees.RupeeCollected += RupeeCollectedHandler;
+    }
+
+    private void RupeeCollectedHandler(object sender, RupeeEvent e)
+    {
+        Rupee rupee = e.Rupee;
+
+        if (rupee)
+        {
+            Value += rupee.Score;
+            OnChanged();
+        }
+    }
+
+    private void OnChanged()
+    {
+        Changed?.Invoke(this, EventArgs.Empty);
+    }
+}
